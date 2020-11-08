0e133fad173d13293e861c554800b75bb46a65df
Christian Doucette <cdoucette826@gmail.com>
2020-11-08 14:02:38 -0600

    Split dataset into train.csv (first 25000 reviews) and test.csv (last 25000 reviews)

--- neural_network.py
+++ neural_network.py
@@ -1,1 +1,12 @@
-print("Neural network code will go here")
+import csv
+
+
+print("Prints out first 10 rows from train.csv")
+with open('train.csv', newline='') as f:
+    reader = csv.reader(f)
+    i = 1
+    for row in reader:
+        print(row)
+        i += 1
+        if i==11:
+            break
