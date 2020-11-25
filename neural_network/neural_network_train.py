bfd27ccd5ecba2a80644f037c3413b0fac49bfaf
Christian Doucette <cdoucette826@gmail.com>
2020-11-25 02:32:17 -0500

    trying to implement RNN, a little stuck but this is what I have for now

--- neural_network/neural_network_train.py
+++ neural_network/neural_network_train.py
@@ -48,7 +48,7 @@ class CSV_Dataset(Dataset):
         labels = []
         for feature,label in parsed_csv:
             features.append(embed_feature(feature))
-            labels.append(int(label))
+            labels.append(int(label=="1"))
 
         self.X = torch.tensor(features)
         self.y = torch.tensor(labels)
@@ -64,10 +64,16 @@ class CSV_Dataset(Dataset):
 
 
 csv_dataset_train = CSV_Dataset('../practice_nn.csv')
-print(csv_dataset_train[2])
 train_data = TensorDataset(csv_dataset_train.X, csv_dataset_train.y)
 train_loader = DataLoader(train_data, batch_size=50, shuffle=True)
+train_iterator = iter(train_loader)
 
+csv_dataset_test = CSV_Dataset('../practice_nn.csv')
+test_data = TensorDataset(csv_dataset_test.X, csv_dataset_test.y)
+test_loader = DataLoader(test_data, batch_size=50, shuffle=True)
+test_iterator = iter(test_loader)
+
+#train_iterator, test_iterator = data.BucketIterator.splits((train_data, test_data), batch_size = 50)
 
 
 class RNN(nn.Module):
@@ -94,3 +100,90 @@ class RNN(nn.Module):
         assert torch.equal(output[-1,:,:], hidden.squeeze(0))
 
         return self.fc(hidden.squeeze(0))
+
+
+
+
+INPUT_DIM = len(id_to_word)
+EMBEDDING_DIM = 100
+HIDDEN_DIM = 256
+OUTPUT_DIM = 1
+
+model = RNN(INPUT_DIM, EMBEDDING_DIM, HIDDEN_DIM, OUTPUT_DIM)
+optimizer = optim.SGD(model.parameters(), lr=1e-3)
+criterion = nn.BCEWithLogitsLoss()
+
+def binary_accuracy(preds, y):
+    """
+    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
+    """
+
+    #round predictions to the closest integer
+    rounded_preds = torch.round(torch.sigmoid(preds))
+    correct = (rounded_preds == y).float() #convert into float for division
+    acc = correct.sum() / len(correct)
+    return acc
+
+
+def train(model, iterator, optimizer, criterion):
+
+    epoch_loss = 0
+    epoch_acc = 0
+
+    model.train()
+
+    for batch in iterator:
+
+        optimizer.zero_grad()
+
+        predictions = model(batch.text).squeeze(1)
+
+        loss = criterion(predictions, batch.label)
+
+        acc = binary_accuracy(predictions, batch.label)
+
+        loss.backward()
+
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+
+
+def evaluate(model, iterator, criterion):
+
+    epoch_loss = 0
+    epoch_acc = 0
+
+    model.eval()
+
+    with torch.no_grad():
+
+        for batch in iterator:
+
+            predictions = model(batch.text).squeeze(1)
+
+            loss = criterion(predictions, batch.label)
+
+            acc = binary_accuracy(predictions, batch.label)
+
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+
+
+N_EPOCHS = 5
+
+best_valid_loss = float('inf')
+
+for epoch in range(N_EPOCHS):
+    train_loss, train_acc = train(model, train_iterator, optimizer, criterion)
+    print(f'Epoch: {epoch}')
+    print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}%')
